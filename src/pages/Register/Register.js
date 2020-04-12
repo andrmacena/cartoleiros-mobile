dcdda4571f41134a996f665ae7ea964c372f2bf5
André Macena <andrmacena_@outlook.com>
2020-04-11 22:39:53 -0300

    Criado a pagina Register e estilizacao do conteudo

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+
+import styles from './styles'
+import logoImg from '../../assets/logo.png'
+
+export default class Register extends Component {
+   render() {
+      return (
+         <View style={styles.container}>
+            <Text>Register</Text>
+         </View>
+      )
+   }
+}
+
+
