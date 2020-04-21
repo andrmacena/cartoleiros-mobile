16ffd61e1c87aeda337319c526412332cd96c2da
André Macena <andrmacena_@outlook.com>
2020-04-20 21:28:18 -0300

    Alterado routes.js, adicionado BottomNavigator para navegacao

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,43 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import Home from './pages/Home/Home'
+import Team from './pages/Team/Team'
 
 const AppStack = createStackNavigator()
+const Bottom = createBottomTabNavigator()
+
+export function BottomTabs() {
+   return (
+      <Bottom.Navigator
+         initialRouteName={Home}
+         tabBarOptions={
+            {
+               activeTintColor: '#e91e63',
+               tabStyle: {
+                  alignContent: 'center',
+                  justifyContent: 'center'
+               }
+
+            }}
+      >
+         <Bottom.Screen name='Home' component={Home} />
+         <Bottom.Screen name='Team' component={Team} />
+      </Bottom.Navigator>
+   )
+}
 
 export default function Routes() {
    return (
       <NavigationContainer>
-         <AppStack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-            <AppStack.Screen name="Login" component={Login} />
-            <AppStack.Screen name="Home" component={Home} />
-            <AppStack.Screen name="Register" component={Register} />
+         <AppStack.Navigator initialRouteName={Login} screenOptions={{ headerShown: false }}>
+            <AppStack.Screen name='Login' component={Login} />
+            <AppStack.Screen name='Register' component={Register} />
+            <AppStack.Screen name='Bottom' component={BottomTabs} />
          </AppStack.Navigator>
       </NavigationContainer>
    )
