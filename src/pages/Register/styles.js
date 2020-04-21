875697d0393211932dc45a042d535f1f3449ae30
André Macena <andrmacena_@outlook.com>
2020-04-20 21:29:37 -0300

    Alterado styles.js da página Register

--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -7,6 +7,40 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+   },
+   containerRegister: {
+      width: '90%',
+      height: '40%',
+      alignItems: 'center',
+      justifyContent: 'center'
+   },
+   titleRegister: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      padding: 20
+   },
+   inputRegister: {
+      backgroundColor: '#fff',
+      width: '90%',
+      height: 45,
+      margin: 15,
+      borderRadius: 8,
+      padding: 10,
+      borderColor: '#000',
+      borderWidth: .3
+   },
+   registerButton: {
+      backgroundColor: '#E04021',
+      width: '90%',
+      height: 45,
+      borderRadius: 8,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 45
+   },
+   textRegisterButton:{
+      fontSize: 20,
+      fontWeight: 'bold'
    }
 })
 
