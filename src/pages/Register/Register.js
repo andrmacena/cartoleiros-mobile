329cdf9b2dad55a08688f4b09568beec294c7d7a
André Macena <andrmacena_@outlook.com>
2020-04-20 21:29:19 -0300

    Alterado Register.js, adicionado funcionalidade de submit dos dados

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,17 +1,50 @@
-import React, { Component } from 'react';
-import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TextInput, Image, TouchableOpacity, useNa } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 import styles from './styles'
 import logoImg from '../../assets/logo.png'
 
-export default class Register extends Component {
-   render() {
-      return (
-         <View style={styles.container}>
-            <Text>Register</Text>
-         </View>
-      )
+import api from '../../services/api'
+
+export default function Register() {
+
+   const [name, setName] = useState('')
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
+
+   const navigation = useNavigation()
+
+   async function submitData() {
+      console.log(name)
+      console.log(email)
+      console.log(password)
+
+      const res = await api.post('users', {
+         name,
+         email,
+         password
+      })
+      console.log(res.message)
+
+      return res ? navigation.goBack() : 'Erro no cadastro'
    }
+
+   return (
+      <View style={styles.container}>
+         <Image source={logoImg} />
+         <Text style={styles.titleRegister}>Cadastrar</Text>
+
+         <TextInput style={styles.inputRegister} placeholder='Nome' onChangeText={name => setName(name)} ></TextInput>
+         <TextInput style={styles.inputRegister} placeholder='Email' onChangeText={email => setEmail(email)} ></TextInput>
+         <TextInput style={styles.inputRegister} placeholder='Senha' onChangeText={password => setPassword(password)} ></TextInput>
+         <TextInput style={styles.inputRegister} placeholder='Confirmar Senha'></TextInput>
+
+         <TouchableOpacity style={styles.registerButton} onPress={submitData}>
+            <Text style={styles.textRegisterButton}>Cadastrar</Text>
+         </TouchableOpacity>
+      </View>
+   )
 }
 
 
