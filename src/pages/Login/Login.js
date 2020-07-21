169c96e65811550dfcb4dd06028185064ae6c24a
André Macena <andrmacena_@outlook.com>
2020-07-20 22:23:06 -0300

    Alterado tela de Login, adicionando função para realização do Logon

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,34 @@
-import React, { Component } from 'react';
-import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TextInput, Image, TouchableOpacity, Alert  } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 
 import styles from './styles'
 import logoImg from '../../assets/logo.png'
 
+import api from '../../services/api'
+
 export default function Login() {
    const navigation = useNavigation()
+
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
+
+   async function submitLogin() {
+
+      try {
+         const res = await api.post('users/authenticate', {
+            email,
+            password
+         })
+   
+         return res.headers !== '' ? navigateToHome() : Alert.alert(res.status)
+         
+      } catch (res) {
+         return Alert.alert('Usuário ou senha inválidos')
+         
+      }
+      
+   }
 
    function navigateToHome() {
       navigation.navigate('Bottom')
@@ -15,18 +37,22 @@ export default function Login() {
    function navigateToRegister(incident) {
       navigation.navigate('Register')
    }
+
+   function navigateToReset() {
+      navigation.navigate('Reset')
+   }
    return (
       <View style={styles.container}>
          <Image source={logoImg} />
          <Text style={styles.titleLogin}>Login</Text>
          <View style={styles.containerLogin}>
-            <TextInput style={styles.inputLogin} placeholder='Email' textContentType={"emailAddress"} keyboardType={"email-address"}></TextInput>
-            <TextInput style={styles.inputLogin} placeholder='Senha' textContentType={"password"} secureTextEntry={true}></TextInput>
-            <TouchableOpacity style={styles.loginButton} onPress={navigateToHome}>
+            <TextInput style={styles.inputLogin} placeholder='Email' onChangeText={email => setEmail(email)} textContentType={"emailAddress"} keyboardType={"email-address"}></TextInput>
+            <TextInput style={styles.inputLogin} placeholder='Senha' onChangeText={password => setPassword(password)} textContentType={"password"} secureTextEntry={true}></TextInput>
+            <TouchableOpacity style={styles.loginButton} onPress={submitLogin}>
                <Text style={styles.textLoginButton}>Entrar</Text>
             </TouchableOpacity>
             <View style={styles.containerOpcoes}>
-               <TouchableOpacity>
+               <TouchableOpacity onPress={navigateToReset}>
                   <Text style={styles.textOpcoes}>Esqueceu sua senha?</Text>
                </TouchableOpacity>
                <TouchableOpacity onPress={navigateToRegister}>
