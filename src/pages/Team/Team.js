9be225a6dedb4f98259b8dc044fcdf8fc64fd6df
André Macena <andrmacena_@outlook.com>
2020-09-27 00:38:53 -0300

    chore: Adicionado layout para adiçao de jogadores

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, Image, TouchableOpacity, ImageBackground } from 'react-native';
 
 import styles from './styles'
-import logoImg from '../../assets/logo.png'
+import campo from '../../assets/campo.jpg'
+import plus from '../../assets/plus-icon.png'
 
 export default class Team extends Component {
    render() {
       return (
          <View style={styles.container}>
-            <Text>Team</Text>
+            <ImageBackground source={campo} style={styles.campo}>
+               <View style={styles.ataque}>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+               </View>
+
+               <View style={styles.meio}>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+               </View>
+
+               <View style={styles.defesa}>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+               </View>
+               <View style={styles.goleiro}>
+                  <TouchableOpacity style={styles.jogadores}>
+                     <Image source={plus} style={styles.imageButtonJogadores}></Image>
+                  </TouchableOpacity>
+               </View>
+
+            </ImageBackground>
          </View>
       )
    }
--- src/pages/Team/styles.js
+++ src/pages/Team/styles.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions, StatusBar } from 'react-native'
 
 const styles = StyleSheet.create({
    container: {
@@ -7,6 +7,41 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+   },
+   campo: {
+      width: Dimensions.get('screen').width - .5,
+      height: Dimensions.get('screen').height - 160,
+      marginTop: 65
+   },
+   jogadores: {
+      height: 50,
+      width: 50,
+      borderWidth: 3,
+      borderRadius: 100,
+      margin: 20,
+   },
+   imageButtonJogadores: {
+      height: '100%',
+      width: '100%',
+      borderRadius: 100
+   },
+   ataque: {
+      flex: 2,
+      flexDirection: 'row',
+      marginLeft: 70
+   },
+   meio: {
+      flex: 2,
+      flexDirection: 'row',
+      marginLeft: 70
+   },
+   defesa: {
+      flex: 2,
+      flexDirection: 'row',
+      marginLeft: 30
+   },
+   goleiro: {
+      marginLeft: 160
    }
 })
 
