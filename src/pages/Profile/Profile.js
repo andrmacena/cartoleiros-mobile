db361765012b26798e33ef9fd1778d2bba47818d
André Macena <andrmacena_@outlook.com>
2020-09-27 00:40:04 -0300

    chore: Criada tela básica para mostrar o perfil/conta do usuário

---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+
+import styles from './styles'
+
+export default class Profile extends Component {
+   render() {
+      return (
+         <View style={styles.container}>
+            <Text>Profile</Text>
+         </View>
+      )
+   }
+}
