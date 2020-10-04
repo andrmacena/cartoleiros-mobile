631707900eecff4a431ae14b22dde4ea8924e2b2
André Macena <andrmacena_@outlook.com>
2020-10-04 20:53:55 -0300

    feat: Adicionado estrutura do layout da tela home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,11 +3,44 @@ import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
 
 import styles from './styles'
 
+import image from '../../assets/plus-icon.png'
+
 export default class Home extends Component {
    render() {
       return (
          <View style={styles.container}>
-            <Text>Home</Text>
+            <View style={styles.infoTeam}>
+               <View style={styles.logoTeamContainer}>
+                  <Image source={image} style={styles.logoTeam} />
+               </View>
+               <View style={styles.infoTeamContainer}>
+                  <Text>Info Team</Text>
+               </View>
+            </View>
+            <View style={styles.infoTeam}>
+               <View style={styles.logoTeamContainer}>
+                  <Image source={image} style={styles.logoTeam} />
+               </View>
+               <View style={styles.infoTeamContainer}>
+                  <Text>Results</Text>
+               </View>
+            </View>
+            <View style={styles.infoTeam}>
+               <View style={styles.logoTeamContainer}>
+                  <Image source={image} style={styles.logoTeam} />
+               </View>
+               <View style={styles.infoTeamContainer}>
+                  <Text>Team of league</Text>
+               </View>
+            </View>
+            <View style={styles.infoTeam}>
+               <View style={styles.logoTeamContainer}>
+                  <Image source={image} style={styles.logoTeam} />
+               </View>
+               <View style={styles.infoTeamContainer}>
+                  <Text>Player of week</Text>
+               </View>
+            </View>
          </View>
       )
    }
