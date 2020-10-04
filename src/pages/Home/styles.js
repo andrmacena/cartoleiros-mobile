0533b465f1a42582a9e4832afb0ae88badfb536e
André Macena <andrmacena_@outlook.com>
2020-10-04 20:54:28 -0300

    feat: Adicionado estilo para o layout da tela home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,8 +4,35 @@ const styles = StyleSheet.create({
    container: {
       flex: 1,
       backgroundColor: '#fff',
+      alignItems: 'center'
+   },
+   infoTeam: {
+      flex: 3,
+      backgroundColor: 'red',
       alignItems: 'center',
-      justifyContent: 'center'
+      borderWidth: 1,
+      borderRadius: 15,
+      height: 120,
+      width: '95%',
+      margin: 10,
+      justifyContent: 'space-between',
+      flexDirection: 'row'
+   },
+   infoTeamContainer: {
+      width: '55%',
+      height: '80%',
+      borderWidth: 1,
+      marginRight: '5%'
+   },
+   logoTeamContainer: {
+      width: '30%',
+      height: '80%',
+      borderWidth: 1,
+      marginLeft: '5%'
+   },
+   logoTeam: {
+      width: '100%',
+      height: '100%'
    }
 })
 
