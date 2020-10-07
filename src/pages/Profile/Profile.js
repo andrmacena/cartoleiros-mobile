8a133fc48d0227a546531e919179d2dae4e2c166
André Macena <andrmacena_@outlook.com>
2020-10-07 20:13:52 -0300

    chore: adicionado estilo a tela de perfil de usuário

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -3,11 +3,23 @@ import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
 
 import styles from './styles'
 
+import image from '../../assets/plus-icon.png'
+
 export default class Profile extends Component {
    render() {
       return (
          <View style={styles.container}>
-            <Text>Profile</Text>
+            <View style={styles.profileInfoImageContainer}>
+               <View style={styles.profileImageContainer}>
+                  <Image source={image} style={styles.profileImage} />
+               </View>
+               <View style={styles.profileInfoContainer}>
+                  <Text>Info sobre a pessoa</Text>
+               </View>
+            </View>
+            <View style={styles.profileInfoContainer1}>
+               <Text>Info sobre a pessoa</Text>
+            </View>
          </View>
       )
    }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -6,7 +6,41 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: 'center'
+   },
+   profileInfoImageContainer: {
+      flex: 1,
+      borderWidth: 1,
+      borderRadius: 15,
+      width: '95%',
+      margin: 10,
+      flexDirection: 'row'
+   },
+   profileImageContainer:{
+      width: '35%',
+      height: '80%',
+      margin: 10,
+      borderColor: 'red',
+      borderWidth: 3
+   },
+   profileImage: {
+      width: '100%',
+      height: '100%'
+   },
+   profileInfoContainer:{
+      width: '50%',
+      height: '80%',
+      margin: 10,
+      borderColor: 'green',
+      borderWidth: 3
+   },
+   profileInfoContainer1: {
+      flex: 2,
+      borderWidth: 1,
+      borderRadius: 15,
+      width: '95%',
+      margin: 10,
+      flexDirection: 'row'
    }
 })
 
