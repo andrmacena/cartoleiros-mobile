f1563acc5612e780c4db5f2ffc4d791045de7215
André Macena <andrmacena_@outlook.com>
2020-12-27 19:18:33 -0300

    feat: Adicionado funcao que chama a api do backend e envia email para realizar o reset da senha

--- src/pages/ResetPassword/ResetPassword.js
+++ src/pages/ResetPassword/ResetPassword.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
-import { Text, View, TextInput, Image, TouchableOpacity, Linking } from 'react-native';
+import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 import styles from './styles'
+import api from '../../services/api'
 import logoImg from '../../assets/logo_size.jpg'
 
 export default function ResetPassword() {
 
    [email, setEmail] = useState('')
+
+   const navigation = useNavigation()
+
+   async function submitData() {
+
+      const res = await api.post('service/send', {
+         email,
+      })
+
+      return res ? navigation.goBack() : Alert.alert('Erro ao enviar email')
+   }
 
    return (
       <View style={styles.container}>
@@ -14,7 +27,7 @@ export default function ResetPassword() {
          <Text style={styles.titleReset}>Restaurar Senha</Text>
          <View style={styles.containerReset}>
             <TextInput style={styles.inputReset} placeholder='Email' textContentType={"emailAddress"} keyboardType={"email-address"} onChangeText={email => setEmail(email)}></TextInput>
-            <TouchableOpacity style={styles.resetButton} onPress={() => { }}>
+            <TouchableOpacity style={styles.resetButton} onPress={submitData}>
                <Text style={styles.textResetButton}>Enviar</Text>
             </TouchableOpacity>
          </View>
