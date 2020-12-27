8d847a4e6c0c7e6594b37cedbd36c2999e466b19
André Macena <andrmacena_@outlook.com>
2020-12-26 23:40:44 -0300

    Alterado Home.js, mudado para componente funcional recebendo os parametros quando usuario logar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,45 +5,47 @@ import styles from './styles'
 
 import image from '../../assets/plus-icon.png'
 
-export default class Home extends Component {
-   render() {
-      return (
-         <View style={styles.container}>
-            <View style={styles.infoTeam}>
-               <View style={styles.logoTeamContainer}>
-                  <Image source={image} style={styles.logoTeam} />
-               </View>
-               <View style={styles.infoTeamContainer}>
-                  <Text>Info Team</Text>
-               </View>
+export default function Home({ route, navigation }) {
+   // render() {
+   const { data } = route.params
+   console.log('home - name logado: ' + data.name)
+   return (
+      <View style={styles.container}>
+         <View style={styles.infoTeam}>
+            <View style={styles.logoTeamContainer}>
+               <Image source={image} style={styles.logoTeam} />
             </View>
-            <View style={styles.infoTeam}>
-               <View style={styles.logoTeamContainer}>
-                  <Image source={image} style={styles.logoTeam} />
-               </View>
-               <View style={styles.infoTeamContainer}>
-                  <Text>Results</Text>
-               </View>
-            </View>
-            <View style={styles.infoTeam}>
-               <View style={styles.logoTeamContainer}>
-                  <Image source={image} style={styles.logoTeam} />
-               </View>
-               <View style={styles.infoTeamContainer}>
-                  <Text>Team of league</Text>
-               </View>
-            </View>
-            <View style={styles.infoTeam}>
-               <View style={styles.logoTeamContainer}>
-                  <Image source={image} style={styles.logoTeam} />
-               </View>
-               <View style={styles.infoTeamContainer}>
-                  <Text>Player of week</Text>
-               </View>
+            <View style={styles.infoTeamContainer}>
+               <Text>info algo</Text>
             </View>
          </View>
-      )
-   }
+         <View style={styles.infoTeam}>
+            <View style={styles.logoTeamContainer}>
+               <Image source={image} style={styles.logoTeam} />
+            </View>
+            <View style={styles.infoTeamContainer}>
+               <Text>Results</Text>
+            </View>
+         </View>
+         <View style={styles.infoTeam}>
+            <View style={styles.logoTeamContainer}>
+               <Image source={image} style={styles.logoTeam} />
+            </View>
+            <View style={styles.infoTeamContainer}>
+               <Text>Team of league</Text>
+            </View>
+         </View>
+         <View style={styles.infoTeam}>
+            <View style={styles.logoTeamContainer}>
+               <Image source={image} style={styles.logoTeam} />
+            </View>
+            <View style={styles.infoTeamContainer}>
+               <Text>Player of week</Text>
+            </View>
+         </View>
+      </View>
+   )
+   // }
 }
 
 
