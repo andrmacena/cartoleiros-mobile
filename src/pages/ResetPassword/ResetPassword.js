8aca518542a69199821b240fa7acc222417ee806
André Macena <andrmacena_@outlook.com>
2020-12-28 21:38:23 -0300

    feat: Adicionado novo componente para realizar o reset da senha

--- src/pages/ResetPassword/ResetPassword.js
+++ src/pages/ResetPassword/ResetPassword.js
@@ -6,32 +6,36 @@ import styles from './styles'
 import api from '../../services/api'
 import logoImg from '../../assets/logo_size.jpg'
 
-export default function ResetPassword() {
+export default function ResetPassword({ route }) {
 
-   [email, setEmail] = useState('')
+   const [email, setEmail] = useState(route.params)
+   const [password, setPassword] = useState('')
 
    const navigation = useNavigation()
 
    async function submitData() {
 
-      const res = await api.post('service/send', {
-         email,
+      const res = await api.put('users/reset', {
+         password,
+         email
       })
 
-      return res ? navigation.goBack() : Alert.alert('Erro ao enviar email')
+      return res ? navigation.navigate('Login') : Alert.alert('Erro ao tentar trocar senha')
    }
 
    return (
       <View style={styles.container}>
          <Image source={logoImg} />
-         <Text style={styles.titleReset}>Restaurar Senha</Text>
+         <Text style={styles.titleReset}>Nova Senha</Text>
          <View style={styles.containerReset}>
-            <TextInput style={styles.inputReset} placeholder='Email' textContentType={"emailAddress"} keyboardType={"email-address"} onChangeText={email => setEmail(email)}></TextInput>
+            <TextInput style={styles.inputReset} placeholder='Email' textContentType={"emailAddress"} keyboardType={"email-address"} defaultValue={email} editable={false} textAlign="center"></TextInput>
+            <TextInput style={styles.inputReset} placeholder='Nova Senha' textContentType={"password"} onChangeText={password => setPassword(password)} secureTextEntry={true}></TextInput>
+            <TextInput style={styles.inputReset} placeholder='Confirmar Senha' textContentType={"password"} secureTextEntry={true}></TextInput>
             <TouchableOpacity style={styles.resetButton} onPress={submitData}>
                <Text style={styles.textResetButton}>Enviar</Text>
             </TouchableOpacity>
          </View>
-      </View>
+      </View >
    )
 }
 
