0eac8e3cc4bc89dce3a7f07bbab1eb4a63d97f73
André Macena <andrmacena_@outlook.com>
2021-01-26 19:55:09 -0300

    feat: Adicionado estado para a tela de Perfil

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,19 +1,30 @@
-import React  from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
 
 import styles from './styles'
 
 export default function Profile({ route, navigation }) {
+   const [name, setName] = useState('')
+   const [email, setEmail] = useState('')
+   const [profile_url, setProfileUrl] = useState('./')
+
    const { data } = route.params
+
+   useEffect(() => { 
+      setName(data.name)
+      setEmail(data.email)
+      setProfileUrl(data.profile_url)
+   })
+
    return (
       <View style={styles.container}>
          <View style={styles.profileInfoImageContainer}>
             <View style={styles.profileImageContainer}>
-               <Image source={{ uri: data.profile_url }} style={styles.profileImage} />
+               <Image source={{ uri: profile_url }} style={styles.profileImage} />
             </View>
             <View style={styles.profileInfoContainer}>
-               <Text>{data.name}</Text>
-               <Text>{data.email}</Text>
+               <Text style={styles.infoText}>{name}</Text>
+               <Text style={styles.infoText}>{email}</Text>
             </View>
          </View>
          <View style={styles.profileFuncionalidades}>
